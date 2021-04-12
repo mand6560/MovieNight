7b3eaa8960a5a86cb94131183b9229ac2fb85d24
Patrick Mandarino <mand6560@mylaurier.ca>
2021-04-12 15:53:44 -0400

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Patrick Mandarino on 2021-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
