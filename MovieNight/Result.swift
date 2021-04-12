3944faee53ecf17e3ffe1304d498d65bd17b3fce
Steven Tran <steven.tran38@hotmail.com>
2021-04-12 17:29:14 -0400

    Added media class

---
+++ MovieNight/Result.swift
@@ -0,0 +1,50 @@
+//
+//  Result.swift
+//  MovieNight
+//
+//  Created by Steven Tran on 2021-04-12.
+//
+
+import Foundation
+import UIKit
+
+class Result {
+    private let title: String
+    private let year: String
+    private let imdbID: String
+    private let mediaType: String
+    private var poster: UIImage?
+    
+    init(title: String, year: String, imdbID: String, mediaType: String, poster: UIImage){
+        self.title = title
+        self.year = year
+        self.imdbID = imdbID
+        self.mediaType = mediaType
+        self.poster = poster
+    }
+    
+    func getTitle() -> String{
+        return self.title
+    }
+    
+    func getYear() -> String{
+        return self.year
+    }
+    
+    func getImdbID() -> String{
+        return self.imdbID
+    }
+    func getMediaType() -> String{
+        return self.mediaType
+    }
+    
+    func getPoster() -> UIImage?{
+        return self.poster
+    }
+    
+    func setPoster(img:UIImage){
+        self.poster = img
+    }
+    
+    
+}
--- MovieNight/SearchViewController.swift
+++ MovieNight/SearchViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate {
+    
+    // MARK: - Global Variable
+    private final var apiKey = "638c2b56"
+    var urlString = "http://www.omdbapi.com/?s="
+    
+    var resultTemp = Result(title: "Spongebob Squarepants", year: "1999", imdbID: "1f4tgg", mediaType: "series", poster: UIImage(systemName: "doc")!)
+    var mediaList = [Result]()
+    
+    var cellIdentifier = "search-cell"
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Search"
-
-        // Do any additional setup after loading the view.
+        mediaList.append(resultTemp)
+        
+    }
+    
+    // MARK: - TableView delgates
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mediaList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = myTableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellIdentifier)
+        }
+        
+        if let img = mediaList[indexPath.row].getPoster(){
+            cell?.imageView?.image = img
+        }
+        cell?.textLabel?.text = mediaList[indexPath.row].getTitle()
+        
+        return cell! //MARK: Change Later
     }
     
 
